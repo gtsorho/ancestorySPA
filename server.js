208681af997d98da9385f37556f08969c44dbced
George Turkson <georgesorho@gmail.com>
2022-02-25 16:02:33 +0000

    second

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
-const { userSetter } = require('core-js/fn/symbol')
 
 const app = express()
 
 app.use('/', serveStatic(path.join(__dirname,'/dist')))
 
-app.get(/.*/, function(req, res){
-    res.sendFile(path.join(__dirname, 'dist/index.html'))
-})
+// app.get(/.*/, function(req, res){
+//     res.sendFile(path.join(__dirname, 'dist/index.html'))
+// })
 const port = process.env.PORT || 8080
 app.listen(port)
 console.log('listening on port:' + port)
